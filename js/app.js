74c1bad30aa7e5ff1f2d33f182f4a897c0a7cbda
Andrey Lompart <andrey.lompart@gmail.com>
2017-04-13 18:36:40 +0200

    added react-router and implemented placeholder for deposits page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import {
+  BrowserRouter as Router,
+  StaticRouter, // for server rendering
+  Route,
+  Link
+} from 'react-router-dom'
+
+import DepositsList from './components/deposits_list'
+console.log(DepositsList)
+
 class App extends React.Component {
   render() {
     console.log("App render()")
-    return <div > Hello {
-      this.props.name
-    } < /div>;
+
+    return <div>
+        <div className="container">
+        </div>
+      </div>
   }
 }
 
-ReactDOM.render( < App name = "Gin 11" / > , document.getElementById('app'));
+class Routes extends React.Component {
+  render() {
+    console.log('Routes');
+
+    return <Router>
+    <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/deposits">Deposits</Link></li>
+        </ul>
+
+        <hr/>
+
+        <Route exact path="/" component={App}/>
+        <Route path="/deposits" component={DepositsList}/>
+      </div>
+    </Router>
+  }
+}
+
+ReactDOM.render(<Routes/>, document.getElementById('app'));
---
+++ js/components/deposits_list.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class DepositsList extends React.Component {
+  render() {
+    return <div>
+      Helloo
+    </div>
+  }
+}
