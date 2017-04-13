af60b81f3a4e47f49578659d53907984281bcc7a
Andrey Lompart <andrey.lompart@gmail.com>
2017-04-13 18:01:45 +0200

    moved out routes from main.go

--- main.go
+++ main.go
@@ -2,70 +2,21 @@ package main
 
 import (
 	"gopkg.in/gin-gonic/gin.v1"
-	"net/http"
-
 	"github.com/go-pg/pg"
-	// "github.com/go-pg/pg/orm"
-	"./models"
-	// "fmt"
- //  "reflect"
 )
 
-// * A saving deposit is identified by a
-// FIELDS:
-// bank name,
-// account number,
-// initial amount saved (currency in USD),
-// start date,
-// end date,
-// interest percentage per year and
-// taxes percentage.
-
-func depositsIndexEndpoint(c *gin.Context) {
-  var deposits []models.Deposit
-
-  err := dbConn.Model(&deposits).Select()
-
-  if err != nil {
-    panic(err)
-  }
-
-	c.JSON(http.StatusOK, gin.H{"deposits": deposits, "count": len(deposits)})
-}
-
-func depositsCreateEndpoint(c *gin.Context) {
-  var deposit models.Deposit
-
-  if c.Bind(&deposit) == nil {
-
-    err := dbConn.Insert(&deposit)
-    if err != nil {
-      panic(err)
-    }
-
-    c.JSON(http.StatusOK, gin.H{"deposit": deposit})
-  }
-
-}
-
 var dbConn *pg.DB
+var router *gin.Engine
 
 func main() {
-	r := gin.Default()
+	router = gin.Default()
 
   dbConn = pg.Connect(&pg.Options{
     User:     "saint",
     Database: "deposit_manager",
   })
 
-	r.Static("/assets", "./assets")
-	r.StaticFile("/", "./assets/index.html")
+  initializeRoutes()
 
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/deposits", depositsIndexEndpoint)
-		v1.POST("/deposits", depositsCreateEndpoint)
-	}
-
-	r.Run(":3001") // listen and serve on 0.0.0.0:8080
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
---
+++ routes.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+  "gopkg.in/gin-gonic/gin.v1"
+  "net/http"
+
+  // "github.com/go-pg/pg"
+  // "github.com/go-pg/pg/orm"
+  "./models"
+  // "fmt"
+ //  "reflect"
+)
+
+// * A saving deposit is identified by a
+// FIELDS:
+// bank name,
+// account number,
+// initial amount saved (currency in USD),
+// start date,
+// end date,
+// interest percentage per year and
+// taxes percentage.
+
+func depositsIndexEndpoint(c *gin.Context) {
+  var deposits []models.Deposit
+
+  err := dbConn.Model(&deposits).Select()
+
+  if err != nil {
+    panic(err)
+  }
+
+  c.JSON(http.StatusOK, gin.H{"deposits": deposits, "count": len(deposits)})
+}
+
+func depositsCreateEndpoint(c *gin.Context) {
+  var deposit models.Deposit
+
+  if c.Bind(&deposit) == nil {
+
+    err := dbConn.Insert(&deposit)
+    if err != nil {
+      panic(err)
+    }
+
+    c.JSON(http.StatusOK, gin.H{"deposit": deposit})
+  }
+
+}
+
+func initializeRoutes() {
+  router.Static("/assets", "./assets")
+  router.StaticFile("/", "./assets/index.html")
+
+  v1 := router.Group("/v1")
+  {
+    v1.GET("/deposits", depositsIndexEndpoint)
+    v1.POST("/deposits", depositsCreateEndpoint)
+  }
+}
